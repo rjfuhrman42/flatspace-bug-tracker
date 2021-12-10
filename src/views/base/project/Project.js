6ab54abc3657598f35ec9d0bd048c7ba2142e180
Reid Fuhrman <reidjf77@gmail.com>
2021-12-09 21:56:00 -0500

    committing old changes

--- src/views/base/project/Project.js
+++ src/views/base/project/Project.js
@@ -7,10 +7,14 @@ const Project = () => {
       <CCardHeader>
         <strong>example-project&#39;s bugs</strong>
       </CCardHeader>
-      <CListGroup></CListGroup>
-      <CListGroupItem component="a" href="#/base/project">
-        #123 error: no list items found...
-      </CListGroupItem>
+      <CCardBody>
+        <CListGroup>
+          <CListGroupItem component="a" href="#/base/project">
+            <h4>Console Error on Website in Chrome</h4>
+            <p style={{ marginBottom: '0' }}>#584 - Opened on: October 2, 2021</p>
+          </CListGroupItem>
+        </CListGroup>
+      </CCardBody>
     </CCard>
   )
 }
